2975b46dce5f1dac7947f53867b2174c9de5f4d5
thomasM <mouchelet.thomas@gmail.com>
2017-10-26 16:49:47 +0200

    add home component, item-list and bootstrap cdn

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ItemListComponent } from './item-list/item-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ItemListComponent
   ],
   imports: [
     BrowserModule

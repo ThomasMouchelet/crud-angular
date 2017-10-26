b2a2315997002147a953f2eea62045d39149f43b
thomasM <mouchelet.thomas@gmail.com>
2017-10-26 17:02:42 +0200

    navbar + routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ItemListComponent } from './item-list/item-list.component';
+
+const routes = [
+  { path: '', component: HomeComponent}
+]
 
 @NgModule({
   declarations: [
@@ -12,7 +18,9 @@ import { ItemListComponent } from './item-list/item-list.component';
     ItemListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
